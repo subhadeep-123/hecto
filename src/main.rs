7d32f1f0dbdf880e2404035dbae3a9bcd1567d8e
Subhadeep Banerjee <subhadeep762@gmail.com>
2025-01-24 13:29:13 +0000

    Enable Raw Mode

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,15 @@
-fn main() {}
+use crossterm::terminal::disable_raw_mode;
+use crossterm::terminal::enable_raw_mode;
+use std::io::{self, Read};
+
+fn main() {
+    enable_raw_mode().unwrap();
+    for b in io::stdin().bytes() {
+        let c = b.unwrap() as char;
+        println!("{}", c);
+        if c == 'q' {
+            disable_raw_mode().unwrap();
+            break;
+        }
+    }
+}
