3bfbbffe49ca323c8283956ac36e433f40f8bd37
Subhadeep Banerjee <subhadeep762@gmail.com>
2025-02-04 11:24:46 +0530

    Fix unsafe arithmetics and casts

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
-#![warn(clippy::all, clippy::pedantic, clippy::print_stdout)]
+#![warn(
+    clippy::all,
+    clippy::pedantic,
+    clippy::print_stdout,
+    clippy::arithmetic_side_effects,
+    clippy::as_conversions,
+    clippy::integer_division
+)]
 use hecto::editor::Editor;
 
 fn main() {
